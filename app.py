b7884c0c112da5ad79af05801cfb74ab251c69ef
AcLec16 <aaravchhaw@gmail.com>
2024-12-02 23:54:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ with sl.form("esg_score_calculator"):
     
     if sl.form_submit_button("Submit"):
          Enviroment 
-         Revenue Efficiency
+         #Revenue Efficiency
        # revenue_efficiency = (total_monthly_revenue - (energy_cost + general_utilities) - intermediate_inputs) / (
             #(energy_cost + general_utilities) - intermediate_inputs
         
